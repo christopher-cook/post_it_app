4334ccf626b3f7128c62b8d619e910cf9aeda020
Qiming Chen <qimingchen91129@gmail.com>
2019-10-25 10:37:04 -0400

    config unit test primitive

---
+++ src/test/com/postit/config/JwtRequestFilterTest.java
@@ -0,0 +1,153 @@
+package com.postit.config;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockFilterChain;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
+
+import com.postit.controller.UserProfileController;
+import com.postit.entity.User;
+import com.postit.entity.UserProfile;
+import com.postit.exception.EmptyFieldException;
+import com.postit.exception.EntityNotFoundException;
+import com.postit.service.UserProfileService;
+import com.postit.service.UserService;
+import com.postit.utils.SecurityUtils;
+import com.postit.config.JwtUtil;
+
+import io.jsonwebtoken.ExpiredJwtException;
+
+public class JwtRequestFilterTest {
+
+  @Rule
+  public MockitoRule rule = MockitoJUnit.rule();
+
+  @InjectMocks
+  UserProfileController userProfileController;
+
+  private MockMvc mockMvc;
+
+  @InjectMocks
+  UserProfile userProfile;
+  
+  @InjectMocks
+  JwtRequestFilter jwtRequestFilter;
+
+  @InjectMocks
+  User user;
+
+  @Mock
+  UserProfileService userProfileService;
+
+  @Mock
+  SecurityUtils securityUtils;
+  
+  @Mock
+  JwtUtil jwtUtil;
+  
+  @Mock
+  UserService userService;
+
+  @Before
+  public void init() {
+
+    mockMvc = MockMvcBuilders.standaloneSetup(userProfileController)
+        .addFilter(jwtRequestFilter).build();
+  }
+
+  @Test
+  public void jwtRequiredRequest_JwtRequestFilter_EmptyToken() throws Exception {
+
+    RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/profile")
+        .contentType(MediaType.APPLICATION_JSON).content(createJson("email1@email.com"));
+
+    MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+    assertEquals("", result.getResponse().getContentAsString());
+  }
+
+  @Test
+  public void jwtRequiredRequest_JwtRequestFilter_IllegalBearerToken() throws Exception {
+
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.post("/profile").contentType(MediaType.APPLICATION_JSON)
+            .content(createJson("email1@email.com")).header("Authorization", "Bearer 12345678910");
+
+    when(jwtUtil.getUsernameFromToken(anyString())).thenThrow(new IllegalArgumentException());
+
+    MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    assertEquals("", result.getResponse().getContentAsString());
+  }
+  
+  @Test
+  public void jwtRequiredRequest_JwtRequestFilter_ExpiredBearerToken() throws Exception {
+
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.post("/profile").contentType(MediaType.APPLICATION_JSON)
+            .content(createJson("email1@email.com")).header("Authorization", "Bearer 12345678910");
+
+    when(userProfileService.getUserProfile(anyString())).thenReturn(null);
+    when(userProfileService.createProfile(anyString(), any())).thenReturn(userProfile);
+    when(jwtUtil.getUsernameFromToken(anyString())).thenThrow(new ExpiredJwtException(null, null, null));
+
+    MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    assertEquals("", result.getResponse().getContentAsString());
+  }
+  
+  @Test
+  public void jwtRequiredRequest_JwtRequestFilter_ValidBearerToken() throws Exception {
+
+    RequestBuilder requestBuilder =
+        MockMvcRequestBuilders.post("/profile").contentType(MediaType.APPLICATION_JSON)
+            .content(createJson("email1@email.com")).header("Authorization", "Bearer 12345678910");
+
+    when(userProfileService.getUserProfile(anyString())).thenReturn(null);
+    when(userProfileService.createProfile(anyString(), any())).thenReturn(userProfile);
+    when(jwtUtil.getUsernameFromToken(anyString())).thenReturn("user1");
+//    when(userService.loadUserByUsername(anyString())).thenReturn(value);
+    
+    MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+    assertEquals("", result.getResponse().getContentAsString());
+  }
+
+  private String createJson(String email) {
+
+    return "{\"additionalEmail\":\"" + email + "\"" + "}";
+  }
+}
---
+++ src/test/com/postit/config/JwtUtilTest.java
@@ -0,0 +1,75 @@
+package com.postit.config;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class JwtUtilTest {
+
+  @Rule
+  public MockitoRule rule = MockitoJUnit.rule();
+
+  @InjectMocks
+  JwtUtil jwtUtil;
+
+  @Mock
+  UserDetails userDetails;
+
+  @Before
+  public void init() {
+
+    jwtUtil.setSecret("test");
+  }
+
+  @Test
+  public void generateToken_TokenString_Success() {
+
+    when(userDetails.getUsername()).thenReturn("user1");
+    String token = jwtUtil.generateToken(userDetails);
+  }
+
+  @Test
+  public void getUsernameFromToken_Username_Success() {
+
+    when(userDetails.getUsername()).thenReturn("user1");
+    String token = jwtUtil.generateToken(userDetails);
+    String username = jwtUtil.getUsernameFromToken(token);
+    assertEquals("user1", username);
+  }
+
+  @Test
+  public void validateToken_Boolean_Success() {
+
+    when(userDetails.getUsername()).thenReturn("user1");
+    String token = jwtUtil.generateToken(userDetails);
+    Boolean isValid = jwtUtil.validateToken(token, userDetails);
+    assertTrue(isValid);
+  }
+
+  @Test
+  public void getExpirationDateFromToken_Date_Success() {
+    when(userDetails.getUsername()).thenReturn("user1");
+//    when(System.currentTimeMillis()).thenReturn(System.currentTimeMillis() - 10 * 60 * 60 * 1000);
+    String token = jwtUtil.generateToken(userDetails);
+    Date expirationDate = jwtUtil.getExpirationDateFromToken(token);
+    assertNotNull(expirationDate);
+//    System.out.println(expirationDate.toString());
+    Date now = new Date(System.currentTimeMillis());
+    assertTrue(now.before(expirationDate));
+  }
+}
